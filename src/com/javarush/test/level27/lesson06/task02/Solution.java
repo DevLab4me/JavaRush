804f2ed881b838324b5a895229de02b878233be3
Artem Bybyk <artem.bybyk@gmail.com>
2015-11-19 10:30:24 +0200

    Initial commit

---
+++ src/com/javarush/test/level27/lesson06/task02/Solution.java
@@ -0,0 +1,161 @@
+package com.javarush.test.level27.lesson06.task02;
+
+/* Определяем порядок захвата монитора. Сложная.
+Реализуйте логику метода isNormalLockOrder, который должен определять:
+соответствует ли порядок synchronized блоков в методе someMethodWithSynchronizedBlocks - порядку
+передаваемых в него аргументов. Должно выполняться условие:
+для разных объектов o1 и o2 isNormalLockOrder(o1, o2) != isNormalLockOrder(o2, o1), для одинаковых объектов одинаковый результат
+Метод main не участвует в тестировании.
+*/
+public class Solution {
+    private static final int DELAY = 25;
+
+    public void someMethodWithSynchronizedBlocks(Object obj1, Object obj2) {
+        //следующие 4 строки в тестах имеют другую реализацию
+        int lock1 = obj1.hashCode();
+        int lock2 = obj2.hashCode();
+        Object firstLock = lock1 > lock2 ? obj1 : obj2;
+        Object secondLock = lock1 > lock2 ? obj2 : obj1;
+
+        synchronized (firstLock) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException ignored) {
+            }
+
+            synchronized (secondLock) {
+                System.out.println(obj1 + " " + obj2);
+            }
+        }
+    }
+
+    public static boolean isNormalLockOrder(final Solution solution, final Object o1, final Object o2) throws Exception {
+        //do something here
+        boolean isSecondMonitorLockedBySomeMethod;
+        synchronized (o1)
+        {
+            synchronized (o2)
+            {
+                Thread checkingThread = new Thread(new Runnable() {
+                    @Override
+                    public void run()
+                    {
+                        solution.someMethodWithSynchronizedBlocks(o1, o2);
+                    }
+                });
+
+                checkingThread.start();
+                // waiting for checkingThread to be blocked on monitors
+                Thread.sleep(2 * DELAY);
+            }
+            // waiting for someMethodWithSynchronizedBlocks to lock second Object
+            Thread.sleep(3 * DELAY);
+
+            isSecondMonitorLockedBySomeMethod = checkSecondMonitorStatus(o2);
+        }
+        Thread.sleep(20 * DELAY); // wait for someMethod to finish
+        return isSecondMonitorLockedBySomeMethod;
+    }
+
+    private static boolean checkSecondMonitorStatus(final Object second)
+    {
+        Thread check = new Thread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                synchronized (second){}
+            }
+        });
+        check.start();
+        try {
+            Thread.sleep(DELAY);
+        } catch (InterruptedException ignored){ }
+
+        return  check.getState().equals(Thread.State.BLOCKED);
+    }
+
+    public static void main(String[] args) throws Exception {
+        final Solution solution = new Solution();
+        final Object o1 = new Object();
+        final Object o2 = new Object();
+
+        String o1s = o1.toString().substring(o1.toString().lastIndexOf("@"));
+        String o2s = o2.toString().substring(o2.toString().lastIndexOf("@"));
+        System.out.println("o1 = " + o1s + "   o2 = " + o2s + "\n");
+
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    String o1s = o1.toString().substring( o1.toString().lastIndexOf("@") );
+                    String o2s = o2.toString().substring( o2.toString().lastIndexOf("@") );
+                    System.out.println("main -> " + this.getName() + " -> isNLO(o1, o2) - " + "("+o1s+", "+o2s+") " +
+                            "; o1.hashCode() > o2.hashCode() = " + (o1.hashCode() > o2.hashCode()) +
+                            " ["+o1.hashCode()+" > "+o2.hashCode()+"] ; isNLO = " +
+                            isNormalLockOrder(solution, o1, o2)  //expected boolean b
+                    );
+                } catch (Exception ignored) {
+                }
+            }
+        }.start();
+
+        Thread.sleep(1000);
+        System.out.println();
+
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    String o1s = o1.toString().substring( o1.toString().lastIndexOf("@") );
+                    String o2s = o2.toString().substring( o2.toString().lastIndexOf("@") );
+                    System.out.println("main -> " + this.getName() + " -> isNLO(o2, o1) - " + "("+o2s+", "+o1s+") " +
+                            "; o2.hashCode() > o1.hashCode() = " + (o2.hashCode() > o1.hashCode()) +
+                            " ["+o2.hashCode()+" > "+o1.hashCode()+"] ; isNLO = " +
+                            isNormalLockOrder(solution, o2, o1) //expected boolean !b
+                    );
+                } catch (Exception ignored) {
+                }
+            }
+        }.start();
+
+        Thread.sleep(1000);
+        System.out.println();
+
+        /* **************************************
+                  test (o1, o1) and (o2, o2)
+           ************************************** */
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    String o1s = o1.toString().substring(o1.toString().lastIndexOf("@")); //test
+                    System.out.println("main -> " + this.getName() + " -> isNLO(o1, o1) - " + "("+o1s+", "+o1s+") " +
+                                    "; o1.hashCode() > o1.hashCode() = " + (o1.hashCode() > o1.hashCode()) +
+                                    " ["+o1.hashCode()+" > "+o1.hashCode()+"] ; isNLO = " + //test
+                                    isNormalLockOrder(solution, o1, o1)
+                    ); //test
+                } catch (Exception ignored) {
+                }
+            }
+        }.start();
+
+        Thread.sleep(1000);
+        System.out.println();
+
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    String o2s = o2.toString().substring( o2.toString().lastIndexOf("@") ); //test
+                    System.out.println("main -> " + this.getName() + " -> isNLO(o2, o2) - " + "("+o2s+", "+o2s+") " +
+                                    "; o2.hashCode() > o2.hashCode() = " + (o2.hashCode() > o2.hashCode()) +
+                                    " ["+o2.hashCode()+" > "+o2.hashCode()+"] ; isNLO = " + //test
+                                    isNormalLockOrder(solution, o2, o2)
+                    ); //test
+                } catch (Exception ignored) {
+                }
+            }
+        }.start();
+    }
+}
