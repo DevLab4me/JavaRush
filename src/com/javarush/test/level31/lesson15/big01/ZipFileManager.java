07deb5704753fdc52e2e3e7c3dd8098792265bd1
Artem Bybyk <artem.bybyk@gmail.com>
2016-04-27 23:06:46 +0300

    Task updates

--- src/com/javarush/test/level31/lesson15/big01/ZipFileManager.java
+++ src/com/javarush/test/level31/lesson15/big01/ZipFileManager.java
@@ -125,9 +125,9 @@ public class ZipFileManager {
 
     private void copyData(InputStream in, OutputStream out) throws Exception {
         byte[] buffer = new byte[8 * 1024];
-        int len;
-        while ((len = in.read(buffer)) > 0) {
-            out.write(buffer, 0, len);
+        int length;
+        while ((length = in.read(buffer)) > 0) {
+            out.write(buffer, 0, length);
         }
     }
 }
