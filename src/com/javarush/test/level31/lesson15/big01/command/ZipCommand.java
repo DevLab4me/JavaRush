51f295a086721dd8ddba13f56d2bad5f5335cd42
Artem Bybyk <artem.bybyk@gmail.com>
2016-04-28 21:52:08 +0300

    Initial commit

--- src/com/javarush/test/level31/lesson15/big01/command/ZipCommand.java
+++ src/com/javarush/test/level31/lesson15/big01/command/ZipCommand.java
@@ -1,8 +1,16 @@
 package com.javarush.test.level31.lesson15.big01.command;
 
-/**
- * Created by Artem on 17.04.2016.
- */
+import com.javarush.test.level31.lesson15.big01.ConsoleHelper;
+import com.javarush.test.level31.lesson15.big01.ZipFileManager;
 
-public abstract class ZipCommand implements Command{
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public abstract class ZipCommand implements Command {
+
+    public ZipFileManager getZipFileManager() throws Exception{
+        ConsoleHelper.writeMessage("Введите полный путь файла архива:");
+        Path zipPath = Paths.get(ConsoleHelper.readString());
+        return new ZipFileManager(zipPath);
+    }
 }
--- src/com/javarush/test/level31/lesson15/big01/command/ZipRemoveCommand.java
+++ src/com/javarush/test/level31/lesson15/big01/command/ZipRemoveCommand.java
@@ -1,12 +1,22 @@
 package com.javarush.test.level31.lesson15.big01.command;
 
-/**
- * Created by Artem on 17.04.2016.
- */
+import com.javarush.test.level31.lesson15.big01.ConsoleHelper;
+import com.javarush.test.level31.lesson15.big01.ZipFileManager;
 
-public class ZipRemoveCommand extends ZipCommand{
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class ZipRemoveCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
+        ConsoleHelper.writeMessage("Удаление файла из архива.");
 
+        ZipFileManager zipFileManager = getZipFileManager();
+
+        ConsoleHelper.writeMessage("Введите полный путь файла в архиве:");
+        Path sourcePath = Paths.get(ConsoleHelper.readString());
+        zipFileManager.removeFile(sourcePath);
+
+        ConsoleHelper.writeMessage("Удаление из архива завершено.");
     }
 }
