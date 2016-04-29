baa575aa7c09678680ceafc62d3d3aa333e1455a
Artem Bybyk <artem.bybyk@gmail.com>
2016-04-29 21:00:57 +0300

    Initial commit

--- src/com/javarush/test/level31/lesson15/big01/command/ZipAddCommand.java
+++ src/com/javarush/test/level31/lesson15/big01/command/ZipAddCommand.java
@@ -1,5 +1,12 @@
 package com.javarush.test.level31.lesson15.big01.command;
 
+import com.javarush.test.level31.lesson15.big01.ConsoleHelper;
+import com.javarush.test.level31.lesson15.big01.ZipFileManager;
+import com.javarush.test.level31.lesson15.big01.exception.PathIsNotFoundException;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * Created by Artem on 17.04.2016.
  */
@@ -7,6 +14,19 @@ package com.javarush.test.level31.lesson15.big01.command;
 public class ZipAddCommand extends ZipCommand{
     @Override
     public void execute() throws Exception {
+        try {
+            ConsoleHelper.writeMessage("Добавление нового файла в архив.");
 
+            ZipFileManager zipFileManager = getZipFileManager();
+
+            ConsoleHelper.writeMessage("Введите полное имя файла для добавления:");
+            Path destinationPath = Paths.get(ConsoleHelper.readString());
+            zipFileManager.addFile(destinationPath);
+
+            ConsoleHelper.writeMessage("Добавление в архив завершено.");
+
+        } catch (PathIsNotFoundException e) {
+            ConsoleHelper.writeMessage("Файл не был найден.");
+        }
     }
 }
