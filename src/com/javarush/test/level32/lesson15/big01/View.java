99cb08ece0d663cabac0d04ad1b46e0009513e43
Artem Bybyk <artem.bybyk@gmail.com>
2016-05-16 22:16:50 +0300

    Initial commit

---
+++ src/com/javarush/test/level32/lesson15/big01/View.java
@@ -0,0 +1,38 @@
+package com.javarush.test.level32.lesson15.big01;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by Artem on 14.05.2016.
+ */
+
+public class View extends JFrame implements ActionListener {
+    private Controller controller;
+    private JTabbedPane tabbedPane = new JTabbedPane();
+    private JTextPane htmlTextPane = new JTextPane();
+    private JEditorPane plainTextPane = new JEditorPane();
+
+
+    public Controller getController() {
+        return controller;
+    }
+
+    public void setController(Controller controller) {
+        this.controller = controller;
+    }
+
+    public void init(){
+
+    }
+
+    public void exit(){
+        controller.exit();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
